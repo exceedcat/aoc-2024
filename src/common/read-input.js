10c89c680b10bad36ec94150ed3e61b8d30fbaf9
Daria Pronina <pronina.dm@gmail.com>
2024-12-02 23:02:17 +0200

    update

--- src/common/read-input.js
+++ src/common/read-input.js
@@ -1,8 +1,7 @@
-import fs from 'fs';
+import fs from "fs";
 
-export const readInput = (day, part) => {
-  const formattedDay = String(day).padStart(2, '0');
-  const path = `../data/${formattedDay}/${formattedDay}-${part}.txt`;
-  return fs.readFileSync(path,
-      'utf8').split('\n');
-}
+export const readInput = (day, part, test) => {
+  const formattedDay = String(day).padStart(2, "0");
+  const path = `src/data/${formattedDay}/${formattedDay}-${part}${test ? "-test" : ""}.txt`;
+  return fs.readFileSync(path, "utf8").split("\n");
+};
